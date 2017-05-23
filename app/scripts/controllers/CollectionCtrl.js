e740cae46d16cfe314fadad2bf70f244ea8f1bb0
Merin Joseph <merinsony18@gmail.com>
2017-05-22 22:12:23 -0700

    assignment 7 service2:completed

--- app/scripts/controllers/CollectionCtrl.js
+++ app/scripts/controllers/CollectionCtrl.js
@@ -1,19 +1,9 @@
 (function() {
      function CollectionCtrl(Fixtures) {
-<<<<<<< HEAD
        this.albums = Fixtures.getCollection(12);
         }
 
      angular
          .module('blocjams')
          .controller('CollectionCtrl',['Fixtures' ,CollectionCtrl]);
-=======
-       this.albums = [];
-       this.albums = Fixtures.getCollection(12);
-     }
-
-     angular
-         .module('blocjams')
-         .controller('CollectionCtrl',['Fixtures', CollectionCtrl]);
->>>>>>> checkpoint-7-service2
  })();
--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -14,33 +14,45 @@
  * @desc Stops currently playing song and loads new audio file as currentBuzzObject
  * @param {Object} song
  */
-          var setSong = function(song) {
+    var setSong = function(song) {
        if (currentBuzzObject) {
            currentBuzzObject.stop();
            currentSong.playing = null;
-       }
+         }
 
        currentBuzzObject = new buzz.sound(song.audioUrl, {
            formats: ['mp3'],
            preload: true
-       });
-
+          });
        currentSong = song;
     };
 
-        SongPlayer.play = function(song) {
-          if (currentSong !== song) {
-            setSong(song);
-            currentBuzzObject.play();
-            song.playing = true;
+    var playSong = function(song) {
+        currentBuzzObject.play();
+        song.playing = true;
+      };
 
+/**
+* @function SongPlayer.play
+* @desc Plays a song when song is selected in the player bar
+* @param {Object} song
+*/
+
+    SongPlayer.play = function(song) {
+        if (currentSong !== song) {
+          setSong(song);
+          playSong(song);
              } else if (currentSong === song) {
-            if (currentBuzzObject.isPaused()) {
+               if (currentBuzzObject.isPaused()) {
                 currentBuzzObject.play();
                   }
-                }
-              };
-
+              }
+          };
+/**
+ * @function SongPlayer.pause
+ * @desc pause a currently playing song when song is selected in the player bar
+ * @param {Object} song
+ */
        SongPlayer.pause = function(song) {
        currentBuzzObject.pause();
        song.playing = false;
